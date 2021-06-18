be08051353ec0bc2a6c70986dc9190d3ce3952c1
Ahmed Mohammed <ahmed1831990@gmail.com>
2021-06-18 20:26:20 +0000

    Automatically backed up by Learn

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -3,6 +3,6 @@ import React from "react"
 
 export default function CoordinatesButton(props){
   return(
-    <button onClick={props.onReceiveCoordinates([event.clientX, event.clientY]);}>Coords</button>;
+    <button onClick={props.onReceiveCoordinates([event.clientX, event.clientY])}>Coords</button>;
   )
 }
