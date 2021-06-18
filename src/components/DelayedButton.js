d6767129523fba0b75dfe8ca0596db1ab8a457d8
Ahmed Mohammed <ahmed1831990@gmail.com>
2021-06-18 20:27:08 +0000

    Automatically backed up by Learn

--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,8 @@
 // Code DelayedButton Component Here
+import React from "react"
+
+export default function DelayedButton(){
+  return(
+    <button onClick={this.handleClick}>Delayed</button>;
+  )
+}
